c1e257d43e888dd3ead42890c8af8c2f7015efaf
Ryan Locascio <63062893+RyanL2020@users.noreply.github.com>
2022-04-14 15:23:39 +0000

    optimized solution

--- script.js
+++ script.js
@@ -5,20 +5,16 @@ let bigString = "abcbdca" //output => 4
 
 const longestSubstring = function(s) {
   if (s.length <= 1) return s.length;
-  let longest = 0;
-  for(let i = 0; i < s.length; i++) {
-    let seenHash = {}, currentLength = 0;
-    for(let right = i; right < s.length; right++) {
-      const currentChar = s[right];
-      if(!seenHash[currentChar]) {
-        currentLength++;
-        seenHash[currentChar] = true;
-        longest = Math.max(longest, currentLength);
-        
-      } else {
-        break;
-      }
+  const seenChar = {};
+  let left = 0, longest = 0;
+  for (let right = 0; right < s.length; right++) {
+    const currentChar = s[right];
+    const prevSeenChar = seenChar[currentChar];
+    if (prevSeenChar >= left) {
+      left = prevSeenChar + 1;
     }
+    seenChar[currentChar] = right;
+    longest = Math.max(longest, right - left + 1);
   }
   return longest;
 }
