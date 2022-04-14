1c59d90c4391c28f3bc4fc58c723a56de5f21252
Ryan Locascio <63062893+RyanL2020@users.noreply.github.com>
2022-04-14 15:13:22 +0000

    brute force solution

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-const buildString = function(string) {
-  const builtArray = [];
-  for(let p = 0; p < string.length; p++) {
-    if (string[p] != '#') {
-      builtArray.push(string[p]);
-    } else {
-      builtArray.pop();
+let strings = "abccabb" //output => 3
+let oneString = "cccccc" //output => 1
+let nullString = "" //output => 0
+let bigString = "abcbdca" //output => 4
+
+const longestSubstring = function(s) {
+  if (s.length <= 1) return s.length;
+  let longest = 0;
+  for(let i = 0; i < s.length; i++) {
+    let seenHash = {}, currentLength = 0;
+    for(let right = i; right < s.length; right++) {
+      const currentChar = s[right];
+      if(!seenHash[currentChar]) {
+        currentLength++;
+        seenHash[currentChar] = true;
+        longest = Math.max(longest, currentLength);
+        
+      } else {
+        break;
+      }
     }
   }
-  return builtArray;
+  return longest;
 }
 
-const backSpaceCompare = function(s, t) {
-  
+console.log(longestSubstring(strings))
